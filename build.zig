c4f526811034b1ea778e41f6baa8c6ef4e43f3d9
Daniel Tralamazza <daniel@sternlabs.org>
2019-06-21 17:45:29 +0200

    that was on purpose

--- build.zig
+++ build.zig
@@ -4,10 +4,10 @@ const builtin = @import("builtin");
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("firmware.elf", "main.zig");
+    const exe = b.addExecutable("firmware.elf", "startup.zig");
     exe.setTarget(builtin.Arch{ .thumb = .v7m }, builtin.Os.freestanding, builtin.Abi.none);
 
-    const main_o = b.addObject("startup", "startup.zig");
+    const main_o = b.addObject("main", "main.zig");
     main_o.setTarget(builtin.Arch{ .thumb = .v7m }, builtin.Os.freestanding, builtin.Abi.none);
     exe.addObject(main_o);
 
